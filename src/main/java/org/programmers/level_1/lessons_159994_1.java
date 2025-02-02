f05e58bf721abc3deb429e8563b9131c5ff64a36
myungyi <wlsaud1992@naver.com>
2025-02-03 00:32:54 +0900

    feat(standard): 159994_1 카드뭉치 문제풀이
    - Queue를 사용한 풀이

---
+++ src/main/java/org/programmers/level_1/lessons_159994_1.java
@@ -0,0 +1,37 @@
+package org.programmers.level_1;
+
+import java.util.ArrayDeque;
+import java.util.Arrays;
+import java.util.Queue;
+
+/**
+ * <a href="https://school.programmers.co.kr/learn/courses/30/lessons/159994">카드 뭉치</a>
+ */
+public class lessons_159994_1 {
+    public String solution(String[] cards1, String[] cards2, String[] goal) {
+        String answer = "Yes";
+        Queue<String> cards1Queue = new ArrayDeque<>(Arrays.asList(cards1));
+        Queue<String> cards2Queue = new ArrayDeque<>(Arrays.asList(cards2));
+        for (int i = 0; i < goal.length ; i++) {
+            String targetGoalStr = goal[i];
+            if (!cards1Queue.isEmpty() && cards1Queue.peek().equals(targetGoalStr)) {
+                cards1Queue.poll();
+            } else if (!cards2Queue.isEmpty() && cards2Queue.peek().equals(targetGoalStr)) {
+                cards2Queue.poll();
+            } else {
+                answer = "No";
+                break;
+            }
+        }
+        return answer;
+    }
+
+    public static void main(String[] args) {
+        lessons_159994_1 lessons = new lessons_159994_1();
+        String[] cards1 = {"i", "drink", "water"};
+//        String[] cards1 = {"i", "water", "drink"};
+        String[] cards2 = {"want", "to"};
+        String[] goal = {"i", "want", "to", "drink", "water"};
+        System.out.println(lessons.solution(cards1, cards2, goal));
+    }
+}
