057924182345024f0a34d5397a848294032a56e0
jmpark <156290869+jmpark7907@users.noreply.github.com>
2025-02-07 18:29:57 +0900

    feat(standard): 340213 동영상_재생기 수정필요

--- src/main/java/org/programmers/level_1/lessons_340213_동영상_재생기.java
+++ src/main/java/org/programmers/level_1/lessons_340213_동영상_재생기.java
@@ -5,13 +5,13 @@ package org.programmers.level_1;
  */
 public class lessons_340213_동영상_재생기 {
     public static String solution(String video_len, String pos, String op_start, String op_end, String[] commands) {
-        int videoLenInt = timeToSeconds(video_len);
-        int posInt = timeToSeconds(pos);
-        int opStartInt = timeToSeconds(op_start);
-        int opEndInt = timeToSeconds(op_end);
+        int videoLenInt = timeToSeconds(video_len);  // 동영상의 길이
+        int posInt = timeToSeconds(pos); // 기능이 수행되기 직전의 재생위치
+        int opStartInt = timeToSeconds(op_start); // 오프닝 시작 시각
+        int opEndInt = timeToSeconds(op_end); // 오프닝이 끝나는 시각
+
         for (String action : commands) {
             if (opStartInt <= posInt && opEndInt >= posInt) {
-                // 현재 위치가 오프닝 구간인 경우, 오프닝이 끝나는 시간으로 이동
                 posInt = opEndInt;
             }
             if (action.equals("next")) {
@@ -24,23 +24,20 @@ public class lessons_340213_동영상_재생기 {
                 }
             } else if (action.equals("prev")) {
                 posInt -= 10;
-                // 10초 이하인 경우
                 if (posInt <= 0) {
                     posInt = 0;
                 }
             }
         }
+        // 사용자의 입력이 모두 끝난 후 동영상의 위치를 "mm:ss" 형식으로 return
         int minuteToSecondsInt = posInt / 60;
         int secondsToInt = posInt % 60;
-        // 사용자의 입력이 모두 끝난 후 동영상의 위치를 "mm:ss" 형식으로 return
         return String.format("%02d:%02d", minuteToSecondsInt, secondsToInt);
     }
 
     private static int timeToSeconds(String timeStr) {
-        String minuteStr = timeStr.split(":")[0];
-        String secondsStr = timeStr.split(":")[1];
-        int minuteToSecondsInt = Integer.parseInt(minuteStr);
-        int secondsToInt = Integer.parseInt(secondsStr);
+        int minuteToSecondsInt = Integer.parseInt(timeStr.split(":")[0]);
+        int secondsToInt = Integer.parseInt(timeStr.split(":")[1]);
         return (minuteToSecondsInt * 60) + secondsToInt;
     }
 
