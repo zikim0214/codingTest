9003ad24e1ba98cfea0eb1407b219212b285e69d
jmpark <156290869+jmpark7907@users.noreply.github.com>
2025-02-07 12:13:16 +0900

    feat(standard): 77484 로또의_최고_순위와_최저_순위 문제풀이

--- src/main/java/org/programmers/level_1/lessons_77484_로또의_최고_순위와_최저_순위.java
+++ src/main/java/org/programmers/level_1/lessons_77484_로또의_최고_순위와_최저_순위.java
@@ -7,27 +7,50 @@ import java.util.Arrays;
  */
 public class lessons_77484_로또의_최고_순위와_최저_순위 {
     public static int[] solution(int[] lottos, int[] win_nums) {
-        int[] answer = {};
         int maximum; // 최대 순위
         int minimum; // 최소 순위
+        int zeroCount = 0;
+        int matchCount = 0;
 
         // 순서와 상관없이, 구매한 로또에 당첨 번호와 일치하는 번호가 있으면 맞힌 걸로 인정
-
         // 로또 번호를 순회하면서 0의 개수와 당첨 번호와 일치하는 번호의 개수 확인
-
         // 순위 계산
+        // 0이 전부 당첨번호 일 가능성도 있음 or 0이 전부 당첨이 안되는 번호 일 수도 있다.
         // 0의 개수를 포함한 최고 순위
         // 0의 개수를 제외한 최저 순위
 
+        // 모두다 0일 경우
+        for (int lotto : lottos) {
+            if (lotto == 0) {
+                zeroCount++;
+            } else if (Arrays.stream(win_nums).anyMatch(num -> num == lotto)) {
+                matchCount++;
+            }
+        }
+        maximum = totalMatchNumber(matchCount + zeroCount);
+        minimum = totalMatchNumber(matchCount);
+        return new int[]{maximum, minimum};
+    }
 
-
-        return answer;
+    // java 14부터 정식으로 도입 switch 표현식
+    private static int totalMatchNumber(int i) {
+        return switch (i) {
+            case 6 -> 1;
+            case 5 -> 2;
+            case 4 -> 3;
+            case 3 -> 4;
+            case 2 -> 5;
+            case 1, 0 -> 6;
+            default -> -1;
+        };
     }
 
     public static void main(String[] args) {
         //  로또 번호와 당첨 번호를 비교하여 최고 순위와 최저 순위를 계산
-        int[] lottos = {44, 1, 0, 0, 31, 25}; // 구매한 로또 번호를 담은 배열
-        int[] win_nums = {31, 10, 45, 1, 6, 19}; // 당첨 번호를 담은 배열
+//        int[] lottos = {44, 1, 0, 0, 31, 25}; // 구매한 로또 번호를 담은 배열
+//        int[] win_nums = {31, 10, 45, 1, 6, 19}; // 당첨 번호를 담은 배열
+        int[] lottos = {0, 0, 0, 0, 0, 0}; // 구매한 로또 번호를 담은 배열
+        int[] win_nums = {38, 19, 20, 40, 15, 25}; // 당첨 번호를 담은 배열
         System.out.println(Arrays.toString(solution(lottos, win_nums)));
     }
 }
