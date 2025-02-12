933091eb796a86e4b8530f0cecbbe62d34b42b30
jmpark <156290869+jmpark7907@users.noreply.github.com>
2025-02-12 19:45:32 +0900

    feat(standard): 181859_배열_만들기6 문제풀이

---
+++ src/main/java/org/programmers/level_0/lessons_181859_배열_만들기6.java
@@ -0,0 +1,37 @@
+package org.programmers.level_0;
+
+
+import java.util.ArrayList;
+import java.util.Arrays;
+
+
+/**
+ * <a href="https://school.programmers.co.kr/learn/courses/30/lessons/181859">배열 만들기6</a>
+ */
+public class lessons_181859_배열_만들기6 {
+    public static int[] solution(int[] arr) {
+        ArrayList<Integer> stk = new ArrayList<>();
+        for (int i = 0; i < arr.length; ) {
+            if (stk.isEmpty()) {
+                stk.add(arr[i]);
+            } else {
+                int endPoint = stk.size() - 1;
+                if (stk.get(endPoint) == arr[i]) {
+                    stk.remove(endPoint);
+                } else {
+                    stk.add(endPoint + 1, arr[i]);
+                }
+            }
+            i++;
+        }
+        return stk.isEmpty() ? new int[]{-1} : stk.stream().mapToInt(Integer::intValue).toArray();
+    }
+
+/*    새로운 배열 stk를 만드려고 한다.
+        i 초기값 0 i 가 arr의 길이보다 작으면 stk*/
+
+    public static void main(String[] args) {
+        int[] arr = {0, 1, 1, 1, 0};
+        System.out.println(Arrays.toString(solution(arr)));
+    }
+}
